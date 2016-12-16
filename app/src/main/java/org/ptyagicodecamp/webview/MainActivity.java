8c07442ef0c0a83f9035904a71174e6f623b3ccb
ptyagi <ptyagicodecamp@gmail.com>
2016-12-16 14:46:30 -0800

    initial commit

---
+++ app/src/main/java/org/ptyagicodecamp/webview/MainActivity.java
@@ -0,0 +1,53 @@
+package org.ptyagicodecamp.webview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import org.ptyagicodecamp.webview_lib.WebViewApi;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+    WebViewApi webViewApi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        //initialize WebViewApi
+        webViewApi = new WebViewApi(webView);
+    }
+
+    public void loadUrl(View view) {
+        webView.loadUrl("https://ptyagicodecamp.github.io/");
+    }
+
+    public void loadData(View view) {
+        webViewApi.loadData("<html><a href=\"https://ptyagicodecamp.github.io/\">Click Here to visit my Blog</html>");
+    }
+
+    public void toggleJavaScript(View view) {
+
+        if (((ToggleButton)view).isChecked()) {
+            webViewApi.enableJS();
+            Toast.makeText(this, "Javascript is enabled", Toast.LENGTH_LONG).show();
+        } else {
+            webViewApi.disableJS();
+            Toast.makeText(this, "Javascript is disabled", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void enableJavaScript(View view) {
+        webViewApi.enableJS();
+    }
+
+    public void disableJavaScript(View view) {
+        webViewApi.disableJS();
+    }
+}
---
+++ webview-android/src/main/java/org/ptyagicodecamp/webview_lib/WebViewApi.java
@@ -0,0 +1,88 @@
+package org.ptyagicodecamp.webview_lib;
+
+import android.util.Log;
+import android.webkit.URLUtil;
+import android.webkit.WebView;
+
+/**
+ * Created by ptyagi on 12/15/16.
+ */
+
+public class WebViewApi {
+    private static final String TAG = "webview";
+
+    private WebView mWebView;
+
+    public WebViewApi(WebView webView) {
+        mWebView = webView;
+    }
+
+    public void loadUrl(String url) {
+        if (URLUtil.isValidUrl(url)) {
+            mWebView.loadUrl(url);
+        } else {
+            Log.e(TAG, "Loaded invalid url in webview");
+        }
+    }
+    public void loadUrl(WebView webView, String url) {
+        mWebView = webView;
+        if (URLUtil.isValidUrl(url)) {
+            webView.loadUrl(url);
+        } else {
+            Log.e(TAG, "Loaded invalid url in webview");
+        }
+    }
+
+    /**
+     * Utility method to enable Javascript in current WebView
+     */
+    public void enableJS() {
+        mWebView.getSettings().setJavaScriptEnabled(true);
+    }
+
+    /**
+     * Utility method to disable Javascript in current WebView
+     */
+    public void disableJS() {
+        mWebView.getSettings().setJavaScriptEnabled(false);
+    }
+
+    /**
+     * Utility method to enable Javascript in a given WebView
+     * @param webView
+     */
+    public void enableJS(WebView webView) {
+        mWebView = webView;
+        webView.getSettings().setJavaScriptEnabled(true);
+    }
+
+    /**
+     * Utility method to disable Javascript in a given WebView
+     * @param webView
+     */
+    public void disableJS(WebView webView) {
+        mWebView = webView;
+        webView.getSettings().setJavaScriptEnabled(false);
+    }
+
+    /**
+     * Returns current active WebView
+     * @return
+     */
+    public WebView getWebView() {
+        return mWebView;
+    }
+
+    /**
+     * Display data in WebView in "text/html" MIIME-type and
+     * UTF-8 encoding
+     * @param data
+     */
+    public void loadData(String data) {
+        if (mWebView instanceof WebView) {
+            mWebView.loadData(data, "text/html", "UTF-8");
+        } else {
+            Log.e(TAG, "WebView not found");
+        }
+    }
+}
